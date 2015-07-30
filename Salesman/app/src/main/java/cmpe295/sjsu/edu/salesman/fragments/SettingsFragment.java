fee6021cc6922f2f0398a69e53cdf0733905011b
jijhaver <jinalijhaveri@gmail.com>
2015-07-29 23:21:49 -0700

    Added settings page

--- Salesman/app/src/main/java/cmpe295/sjsu/edu/salesman/fragments/SettingsFragment.java
+++ Salesman/app/src/main/java/cmpe295/sjsu/edu/salesman/fragments/SettingsFragment.java
@@ -1,9 +1,23 @@
 package cmpe295.sjsu.edu.salesman.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import cmpe295.sjsu.edu.salesman.R;
 
 /**
  * Created by jijhaver on 6/22/15.
  */
 public class SettingsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.settings_fragment, container, false);
+        return rootView;
+    }
 }
